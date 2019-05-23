cbf88596e84bbe33fd2a2d6a4a6418e42f2f06fc
Herpiko Dwi Aguno <herpiko@gmail.com>
2019-05-24 06:57:44 +0700

    notokyet

--- cli-rust/src/main.rs
+++ cli-rust/src/main.rs
@@ -7,8 +7,9 @@ use clap::{App, Arg, SubCommand};
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let author =
         "BlankOn Developer <blankon-dev@googlegroups.com>\nHerpiko Dwi Aguno <herpiko@aguno.xyz>";
     let version = "0.0.1";
@@ -44,6 +45,7 @@ fn main() {
     										.long("source")
     										.value_name("URL")
     										.help("Source Git URL")
+                        .required(true)
     										.takes_value(true)))
     								.subcommand(SubCommand::with_name("status")
     									.about("Checks the status of a pipeline.")
@@ -80,7 +82,7 @@ fn main() {
             "Successfully sets the chief address to {}. Now you can use irgsh-cli.",
             matches.value_of("chief").unwrap()
         );
-        return;
+        return Ok(());
     }
 
     config_file.push_str("/.irgsh/IRGSH_CHIEF_ADDRESS");
@@ -88,36 +90,76 @@ fn main() {
 
     if let Some(matches) = matches.subcommand_matches("submit") {
         println!("Chief       : {}", chief_url);
-        println!("Source URL : {}", matches.value_of("source").unwrap());
-        println!("Package URL : {}", matches.value_of("package").unwrap());
+				if matches.value_of("source").unwrap().chars().count() > 0 {
+        	println!("Source URL  : {}", matches.value_of("source").unwrap());
+				}
+				if matches.value_of("package").unwrap().chars().count() > 0 {
+        	println!("Package URL : {}", matches.value_of("package").unwrap());
+				}
+    		let mut chief_url = fs::read_to_string(&config_file).expect("Unable to read config file. Please initialize the irgsh-cli first. See --help for further information.");
         chief_url.push_str("/api/v1/submit");
         println!("Submit URL  : {}", chief_url);
 
-    		let echo_json: serde_json::Value = reqwest::Client::new()
-    		    .post(chief_url)
-    		    .json(
-    		        &json!({
-                    "sourceUrl": matches.value_of("source").unwrap(),
-                    "packageUrl": matches.value_of("package").unwrap()
-    		        })
-    		    )
-    		    .send()?
-    		    .json();
+    		let payload: serde_json::Value = json!({
+            "sourceUrl": matches.value_of("source").unwrap(),
+            "packageUrl": matches.value_of("package").unwrap()
+    		});
 
-        println!("{:#?}", echo_json);
-        return;
+				let _result: serde_json::Value = match post(chief_url, payload) {
+					Ok(result) => result,
+					Err(_e) => return Ok(())
+				};
+
+        return Ok(());
+
     } else if let Some(matches) = matches.subcommand_matches("status") {
         println!(
             "Status of PipelineID: {}",
             matches.value_of("PIPELINE_ID").unwrap()
         );
+        return Ok(());
     } else if let Some(matches) = matches.subcommand_matches("watch") {
         println!(
             "Watching PipelineID: {}",
             matches.value_of("PIPELINE_ID").unwrap()
         );
+        return Ok(());
     // Fall back to status subcommand if the pipeline was done.
     } else {
         println!("\nPlease run by a subcommand. See --help for further information.");
+        return Ok(());
     }
+}
+
+
+fn post(url: String, json_payload: serde_json::Value) -> Result<serde_json::Value, reqwest::Error> {
+    		let echo_json: serde_json::Value = reqwest::Client::new()
+    		    .post(&url)
+    		    .json(&json_payload
+    		    )
+    		    .send()?
+            .json()?;
+
+          println!("{:#?}", echo_json);
+          return Ok(echo_json)
+}
+
+fn handler(e: reqwest::Error) {
+   if e.is_http() {
+       match e.url() {
+           None => println!("No Url given"),
+           Some(url) => println!("Problem making request to: {}", url),
+       }
+   }
+   // Inspect the internal error and output it
+   if e.is_serialization() {
+      let serde_error = match e.get_ref() {
+           None => return,
+           Some(err) => err,
+       };
+       println!("problem parsing information {}", serde_error);
+   }
+   if e.is_redirect() {
+       println!("server redirecting too many times or making loop");
+   }
 }
